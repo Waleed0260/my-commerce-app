aaaaea92c15b1881ae8ec11850c0a6bab95b58c8
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-02-27 12:15:33 -0800

    changes

--- src/Components/HOC.jsx
+++ src/Components/HOC.jsx
@@ -11,7 +11,7 @@ function UpdatedComponent(OriginalComponent) {
         let params = useParams();
     
         const getProducts = async(name)=>{
-            const url= `https://api.spoonacular.com/food/products/search?apiKey=2777352d167849208b58bd3897cbed6e&query=${name}&vegetables&number=20`
+            const url= `https://api.spoonacular.com/food/products/search?apiKey=e2d392617b924963a2edb8e0e7a61b59&query=${name}&vegetables&number=20`
             const response = await fetch(url);
             const resJson = await response.json();
             setProducts(resJson.products);
